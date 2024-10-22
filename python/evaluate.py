87dabd8c1d67077245a1a72b8e672f76db33c33f
Isabel <isabel.martinez1@bsc.es>
2024-10-22 17:18:04 +0200

    finishing function to fit real data

--- python/evaluate.py
+++ python/evaluate.py
@@ -14,6 +14,7 @@ def fit_epicurves(simdata_ds, instance_folder, data_folder,
                   epidata_fname=None, epi_variable=None,
                   level='global', **kwargs):
     
+    
     config_fname = os.path.join(instance_folder, "config.json")
     with open(config_fname) as fh:
         config_dict = json.load(fh)
@@ -23,14 +24,23 @@ def fit_epicurves(simdata_ds, instance_folder, data_folder,
 
     epidata_fname = os.path.join(data_folder, epidata_fname)
     epidata_ds = xr.load_dataset(epidata_fname)
+
+    epidata_ds['T'] = pd.DatetimeIndex(epidata_ds['T'].values)
+    epidata_ds = epidata_ds.sel(T=slice( start_date,end_date))
+    simdata_ds['T'] = pd.DatetimeIndex(simdata_ds['T'].values)
     
     epidata_xa = epidata_ds[epi_variable]
     simdata_xa = simdata_ds[epi_variable]
 
+    output_folder = os.path.join(instance_folder, "output")
+    output_fname  = os.path.join(output_folder, kwargs["output_fname"])
+
     if level == 'global':
-        epidata_xa = epidata_ds.sum(['G', 'M'])
+        epidata_xa = epidata_xa.sum(['G', 'M'])
         simdata_xa = simdata_xa.sum(['G', 'M'])
-        return xs.rmse(simdata_xa, epidata_xa, dim = 'T')
+        rmse_xa = xs.rmse(simdata_xa, epidata_xa, dim = 'T')
+        rmse_xa.to_netcdf(output_fname)
+        return float(rmse_xa)
 
 def dummy_evalaute(sim_xa, instance_folder, data_folder, **kwargs):
     return 0

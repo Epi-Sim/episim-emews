47305079fe7ae4cff3927579289631f215425146
Isabel <isabel.martinez1@bsc.es>
2025-02-05 17:54:42 +0100

    generalizing summary of experiment by adding other metrics(MAPE)

--- scripts/summary_experiment_mo.py
+++ scripts/summary_experiment_mo.py
@@ -4,17 +4,18 @@ import xarray as xr
 import xskillscore as xs
 import json
 
-experiment_folder = "test_deap_ga_mo"
+experiment_folder = "test_deap_cmaes_mo_prueba"
 level = "global"
 prov = '02'
-pattern = f"../experiments/{experiment_folder}/instance_*/output/RMSE_{level}.nc"
+metric = 'MAPE'
+pattern = f"../experiments/{experiment_folder}/instance_*/output/{metric}_{level}.nc"
 
 list_gen = []
 list_ind = []
 
 if level == 'global':
-	list_RMSE_D = []
-	list_RMSE_H = []
+	list_D = []
+	list_H = []
 	for path in glob.glob(pattern):
 	    split_path = path.split("/")
 	    instance = split_path[3]
@@ -22,23 +23,26 @@ if level == 'global':
 	    generation = split_instance[1]
 	    individual = split_instance[2]
 
-	    rmse = xr.load_dataarray(path)
-	    RMSE_D = rmse['new_deaths']
-	    RMSE_H = rmse['new_hospitalized']
+	    metric = xr.load_dataset(path)
+	    metric_D = metric['new_deaths']
+	    metric_H = metric['new_hospitalized']
 	    list_gen.append(float(generation))
 	    list_ind.append(float(individual))
-	    list_RMSE_D.append(float(RMSE_D))
-	    list_RMSE_H.append(float(RMSE_H))
-	    
-	d = {'generation': list_gen,'individual': list_ind,
-	     'RMSE_D': list_RMSE_D, 'RMSE_H': list_RMSE_H}
+	    list_D.append(float(metric_D))
+	    list_H.append(float(metric_H))
+	if metric == 'RMSE': 
+		d = {'generation': list_gen,'individual': list_ind,
+	     	'RMSE_D': list_metric_D, 'RMSE_H': list_metric_H}
+	elif metric == 'MAPE':
+		d = {'generation': list_gen,'individual': list_ind,
+	     	'MAPE_D': list_metric_D, 'MAPE_H': list_metric_H}
 
 elif level == 'age':
-	list_RMSE_Y_H = []
-	list_RMSE_M_H = []
-	list_RMSE_O_H = []
-	list_RMSE_M_D = []
-	list_RMSE_O_D = []
+	list_Y_H = []
+	list_M_H = []
+	list_O_H = []
+	list_M_D = []
+	list_O_D = []
 	for path in glob.glob(pattern):
 	    split_path = path.split("/")
 	    instance = split_path[3]
@@ -46,27 +50,32 @@ elif level == 'age':
 	    generation = split_instance[1]
 	    individual = split_instance[2]
 
-	    rmse = xr.load_dataset(path)
-	    Y_H = rmse['new_hospitalized'].loc['Y']
-	    M_H = rmse['new_hospitalized'].loc['M']
-	    D_H = rmse['new_hospitalized'].loc['O']
-	    M_D = rmse['new_deaths'].loc['M']
-	    O_D = rmse['new_deaths'].loc['O']
+	    metric = xr.load_dataset(path)
+	    Y_H = metric['new_hospitalized'].loc['Y']
+	    M_H = metric['new_hospitalized'].loc['M']
+	    D_H = metric['new_hospitalized'].loc['O']
+	    M_D = metric['new_deaths'].loc['M']
+	    O_D = metric['new_deaths'].loc['O']
 	    list_gen.append(float(generation))
 	    list_ind.append(float(individual))
-	    list_RMSE_Y_H.append(float(Y_H))
-	    list_RMSE_M_H.append(float(M_H))
-	    list_RMSE_O_H.append(float(O_H))
-	    list_RMSE_M_D.append(float(M_D))
-	    list_RMSE_O_D.append(float(O_D))
-	    
-	d = {'generation': list_gen,'individual': list_ind,'RMSE_Y_hosp': list_RMSE_Y_H,
-		'RMSE_M_hosp': list_RMSE_M_H, 'RMSE_O_hosp': list_RMSE_O_H,
-		'RMSE_M_death': list_RMSE_M_D, 'RMSE_O_death': list_RMSE_O_D}
+	    list_Y_H.append(float(Y_H))
+	    list_M_H.append(float(M_H))
+	    list_O_H.append(float(O_H))
+	    list_M_D.append(float(M_D))
+	    list_O_D.append(float(O_D))
+	
+	if metric == 'RMSE':	    
+	    d = {'generation': list_gen,'individual': list_ind,'RMSE_Y_hosp': list_Y_H,
+		'RMSE_M_hosp': list_M_H, 'RMSE_O_hosp': list_O_H,
+		'RMSE_M_death': list_M_D, 'RMSE_O_death': list_O_D}
+	elif metric == 'RMSE':	    
+	    d = {'generation': list_gen,'individual': list_ind,'MAPE_Y_hosp': list_Y_H,
+		'MAPE_M_hosp': list_M_H, 'MAPE_O_hosp': list_O_H,
+		'MAPE_M_death': list_M_D, 'MAPE_O_death': list_O_D}
 		
 elif level == 'prov':
-	list_RMSE_D = []
-	list_RMSE_H = []
+	list_D = []
+	list_H = []
 	for path in glob.glob(pattern):
 	    split_path = path.split("/")
 	    instance = split_path[3]
@@ -74,52 +83,61 @@ elif level == 'prov':
 	    generation = split_instance[1]
 	    individual = split_instance[2]
 
-	    rmse = xr.load_dataset(path)
-	    RMSE_D = rmse['new_deaths'].loc[prov]
-	    RMSE_H = rmse['new_hospitalized'].loc[prov]
+	    metric = xr.load_dataset(path)
+	    metric_D = metric['new_deaths'].loc[prov]
+	    metric_H = metric['new_hospitalized'].loc[prov]
 	    list_gen.append(float(generation))
 	    list_ind.append(float(individual))
-	    list_RMSE_D.append(float(RMSE_D))
-	    list_RMSE_H.append(float(RMSE_H))
-	    d = {'generation': list_gen,'individual': list_ind,
-	     'RMSE_D': list_RMSE_D, 'RMSE_H': list_RMSE_H}
-	    level = f'prov_{prov}'
-
-elif level == 'prov_age'
-	list_RMSE_Y_H = []
-	list_RMSE_M_H = []
-	list_RMSE_O_H = []
-	list_RMSE_M_D = []
-	list_RMSE_O_D = []
-	for path in glob.glob(pattern):
-	    split_path = path.split("/")
-	    instance = split_path[3]
-	    split_instance = instance.split("_")
-	    generation = split_instance[1]
-	    individual = split_instance[2]
-
-	    rmse = xr.load_dataset(path)
-	    Y_H = rmse['new_hospitalized'].loc[prov,'Y']
-	    M_H = rmse['new_hospitalized'].loc[prov,'M']
-	    D_H = rmse['new_hospitalized'].loc[prov,'O']
-	    M_D = rmse['new_deaths'].loc[prov,'M']
-	    O_D = rmse['new_deaths'].loc[prov,'O']
-	    list_gen.append(float(generation))
-	    list_ind.append(float(individual))
-	    list_RMSE_Y_H.append(float(Y_H))
-	    list_RMSE_M_H.append(float(M_H))
-	    list_RMSE_O_H.append(float(O_H))
-	    list_RMSE_M_D.append(float(M_D))
-	    list_RMSE_O_D.append(float(O_D))
-	    level = f'prov_{prov}'
+	    list_D.append(float(metric_D))
+	    list_H.append(float(metric_H))
 	    
-	d = {'generation': list_gen,'individual': list_ind,'RMSE_Y_hosp': list_RMSE_Y_H,
-		'RMSE_M_hosp': list_RMSE_M_H, 'RMSE_O_hosp': list_RMSE_O_H,
-		'RMSE_M_death': list_RMSE_M_D, 'RMSE_O_death': list_RMSE_O_D}	
+	 if metric == 'RMSE':
+	    d = {'generation': list_gen,'individual': list_ind,
+	     'RMSE_D': list_D, 'RMSE_H': list_H}
+	 elif metric == 'MAPE':
+	    d = {'generation': list_gen,'individual': list_ind,
+	     'MAPE_D': list_D, 'MAPE_H': list_H}
+	 level = f'prov_{prov}'
+
+elif level == 'prov_age':
+	list_Y_H = []
+	list_M_H = []
+	list_O_H = []
+	list_M_D = []
+	list_O_D = []
+	for path in glob.glob(pattern):
+	    split_path = path.split("/")
+	    instance = split_path[3]
+	    split_instance = instance.split("_")
+	    generation = split_instance[1]
+	    individual = split_instance[2]
+
+	    metric = xr.load_dataset(path)
+	    Y_H = metric['new_hospitalized'].loc[prov,'Y']
+	    M_H = metric['new_hospitalized'].loc[prov,'M']
+	    D_H = metric['new_hospitalized'].loc[prov,'O']
+	    M_D = metric['new_deaths'].loc[prov,'M']
+	    O_D = metric['new_deaths'].loc[prov,'O']
+	    list_gen.append(float(generation))
+	    list_ind.append(float(individual))
+	    list_Y_H.append(float(Y_H))
+	    list_M_H.append(float(M_H))
+	    list_O_H.append(float(O_H))
+	    list_M_D.append(float(M_D))
+	    list_O_D.append(float(O_D))
+	    level = f'prov_{prov}'
+	if metric == 'RMSE':    
+	    d = {'generation': list_gen,'individual': list_ind,'RMSE_Y_hosp': list_Y_H,
+		'RMSE_M_hosp': list_M_H, 'RMSE_O_hosp': list_O_H,
+		'RMSE_M_death': list_M_D, 'RMSE_O_death': list_O_D}
+	elif metric == 'MAPE':    
+	    d = {'generation': list_gen,'individual': list_ind,'MAPE_Y_hosp': list_Y_H,
+		'MAPE_M_hosp': list_M_H, 'MAPE_O_hosp': list_O_H,
+		'MAPE_M_death': list_M_D, 'MAPE_O_death': list_O_D}		
 
 df = pd.DataFrame(data = d)
 
 #df = df.sort_values(by=['RMSE_M_death', 'RMSE_O_death', 'RMSE_Y_hosp'])
 
-output_path = f"../experiments/{experiment_folder}/summary_{level}.csv"
+output_path = f"../experiments/{experiment_folder}/summary_{metric}_{level}.csv"
 df.to_csv(output_path, index=False)   
